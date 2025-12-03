f1c12c5294c710a4d131654798b05fc7ebac3b74
Elinor12w <elinor12w@gmail.com>
2025-12-03 18:59:04 +0200

    new file

---
+++ lesson 1/loops.js
@@ -0,0 +1,23 @@
+
+console.log("hello world it is me elinor ")
+
+//1
+function TEN_loop(name1){
+    for (let i = 0; i < 10; i++) {
+        console.log(name1);
+    }
+}
+
+let name = prompt("Enter your name:");
+TEN_loop(name);
+
+
+function x_loop(number){
+    for (let i = 0; i < number; i++)
+    {
+        console.log("hi,", i + 1);
+    }
+}
+const num = prompt("Enter a number:");
+x_loop(num);
+
