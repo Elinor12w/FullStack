f70c413095fed3ea7ffa5b1b03f5883af8fc5c43
Elinor12w <elinor12w@gmail.com>
2025-12-04 12:57:25 +0200

    did 4-5

--- lesson 1/loops.js
+++ lesson 1/loops.js
@@ -36,3 +36,43 @@ return sum/x1;
 }
 const x= Number(prompt("enter a num"));
 console.log( "the marks avrege is ", x_marks_avrgege(x));
+
+//4
+function multiplication_formula(n){
+
+    for (let i = 1; i <=10 ;i++)
+        {
+        for(let j = 1; j <=n;j++){
+
+            console.log(i*j+"\t");
+   }
+}}
+const num2=prompt("enter a number for multiplication table:");
+console.log(multiplication_formula(num2));
+
+//5c 
+ function loop_words(){
+    let word= prompt("enter a word (type 'stop' to end):");
+while(word !== "stop" )
+    {  console.log(word);
+     word = prompt("enter a word (type 'stop' to end):");
+  
+}
+
+} loop_words();
+
+//6
+function max_number(){
+    let max =0
+    use_number = Number(prompt("enter a number (type '-1' to end):"));
+    while(use_number !== -1){
+        if (use_number>max){
+            max = use_number;
+        }
+        use_number = Number(prompt("enter a number (type '-1' to end):"));
+    }
+return max;
+}
+console.log("the max num is", max_number());   
+
+//7
