fadc66ae0bc185eafc6902c80f5c0098c1244fe7
Elinor12w <elinor12w@gmail.com>
2025-12-09 20:59:06 +0200

    finished sub 2

--- lesson 2/jsdom.js
+++ lesson 2/jsdom.js
@@ -16,6 +16,7 @@ document.addEventListener("mousemove", function(event) {
 })
 //3
 const showButton=document.querySelector(".showEmgBtn")
+//4
 const inputEmg= document.querySelector(".imageinpute")
 const container= document.querySelector(".imageContainer")
 showButton.addEventListener("click",function(){
@@ -26,7 +27,7 @@ showButton.addEventListener("click",function(){
 
 
 })
-//4
+//5
 const remove_button= document.querySelector(".removeBtn")
 const num_arr=[1,2,3,4,5,6,7,8,9]
 console.log("the arr:",num_arr)
@@ -35,3 +36,25 @@ remove_button.addEventListener("click",function(){
     num_arr.pop()
     console.log("the arr after remove:",num_arr)
 })
+
+const four_butoons=document.querySelectorAll(".color_btn")
+four_butoons.forEach(function(button)
+{
+    button.addEventListener("click",function()
+    {
+        const color= button.getAttribute("data_color")
+       document.body.style.backgroundColor = color;
+    })
+
+    })
+
+    //6
+    const arr_colors=["red","green","blue","yellow","pink","black"]
+    function randm_colorfunc(){
+        const index= Math.floor(Math.random()* arr_colors.length)    
+        return arr_colors[index]
+    }
+    setInterval(function(){
+        const radom_color= randm_colorfunc();
+        document.body.style.backgroundColor= radom_color;
+    }, 2000)
